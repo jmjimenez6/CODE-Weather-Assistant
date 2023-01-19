5d2964079beb61223cf3ddca9a4514e854c93ee8
Eduardo (David) Villacis Calderon <villacis@vt.edu>
2023-01-19 13:32:04 -0700

    Adding coding template

---
+++ code_1.py
@@ -0,0 +1,11 @@
+#Name
+#CIS 3330
+#CODE 1 - Temperature Assistant
+#Conversion formula: (Temperature in °F - 32) * .5556
+#Message to user "What is the temperature outside:"
+
+def main():
+     pass#replace this line with your code
+
+if __name__ == "__main__":
+    main()
